f0a6c47f6e7fb292fe69cb2892d1b1ab9e5669b3
cj <christopherwjohnson@gmail.com>
2016-10-16 21:36:11 -0600

    Working on loading schema JSON.  Added commoner.

--- projects/trellis_json/source/trellis_json/serialize.cpp
+++ projects/trellis_json/source/trellis_json/serialize.cpp
@@ -1,11 +1,32 @@
 #include "serialize.h"
 #include <vineyard/Trellis.h>
 
+#include "rapidjson/document.h"
+#include <rapidjson/istreamwrapper.h>
+#include <iostream>
+#include <fstream>
+
+using namespace rapidjson;
+using namespace std;
 using namespace vineyard;
 
 namespace trellis_json {
 
-  void load(const std::string &filename, const Trellis_Delegate &delegate) {
+  void load_trellises(const Value &trellises, vector<unique_ptr<Trellis>> &trellis_buffer) {
+    trellis_buffer.reserve(trellises.GetArray().Size());
 
+    for (auto &trellis_node: trellises.GetArray()) {
+      trellis_buffer.push_back(unique_ptr<Trellis>(new Trellis(trellis_node["name"].GetString())));
+      auto &trellis = trellis_buffer[trellis_buffer.size() - 1];
+    }
+  }
+
+  void load(const std::string &filename, vector<unique_ptr<Trellis>> &trellis_buffer) {
+    ifstream file_stream(filename);
+    IStreamWrapper stream_wrapper(file_stream);
+    Document document;
+    document.ParseStream(stream_wrapper);
+
+    load_trellises(document["trellises"], trellis_buffer);
   }
 }
--- projects/trellis_json/source/trellis_json/serialize.h
+++ projects/trellis_json/source/trellis_json/serialize.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace vineyard {
   class Trellis;
@@ -12,5 +13,5 @@ namespace trellis_json {
 
   using Trellis_Delegate =std::function<void(const std::unique_ptr<vineyard::Trellis> &)>;
 
-  void load(const std::string &filename, const Trellis_Delegate &delegate);
+  void load(const std::string &filename, std::vector<std::unique_ptr<vineyard::Trellis>> &trellis_buffer);
 }
